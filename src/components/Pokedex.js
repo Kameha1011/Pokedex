e1bd861fad90619b13fdaad0299b334dd0713a6e
kameha1011 <omarcitofc2001@gmail.com>
2022-12-20 21:34:27 -0300

    react status and request code changed

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -7,7 +7,6 @@ const customOptions = { cacheImages: true }
 const P = new PokeApi.Pokedex(customOptions);
 // from 387 to 493 4th gen pokemons
 
-let allPokemonData = [];
 const pokemonFinder = async (id) => {
     try {
        const data = await P.getPokemonByName(id);
@@ -17,35 +16,72 @@ const pokemonFinder = async (id) => {
         console.log(error);
     }
 };
-const getPkmnData = async (from, to) =>{ 
-    try {
-        for (let i = from; i <= to; i++) {
-            const data = await pokemonFinder(i);
-            let pokemonData = {};
-            pokemonData.name = data.name;
-            pokemonData.type = data.types[0].type.name;
-            pokemonData.baseExp = data.base_experience;
-            pokemonData.sprite = data.sprites.front_default;
-            allPokemonData.push(pokemonData);
-       }
-       return allPokemonData;
-    } catch (error) {
-        console.log(error);
-    }
-};
-getPkmnData(1,9);
+// const getPkmnData = async (from, to) =>{ 
+//     try {
+//         for (let i = from; i <= to; i++) {
+//             const data = await pokemonFinder(i);
+//             let pokemonData = {};
+//             pokemonData.name = data.name;
+//             pokemonData.type = data.types[0].type.name;
+//             pokemonData.baseExp = data.base_experience;
+//             pokemonData.sprite = data.sprites.front_default;
+//             allPokemonData.push(pokemonData);
+//        }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+// getPkmnData(1,9);
+// console.log(allPokemonData);
 
 const capitalizeFirstLetter = (word) => {
     const newWord = word.charAt(0).toUpperCase() + word.slice(1);
     return newWord
 };
 class Pokedex extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          pokemonDatas: [],
+        };
+      }
+    async componentDidMount() {
+        try {
+          //let pokemones = [];
+          let arrayRequest = [];
+          
+          for (let i = 387; i <= 493; i++) {
+            const data = pokemonFinder(i);
+            arrayRequest.push(data);
+            //let pokemonData = {};
+            //pokemonData.name = data.name;
+            //pokemonData.type = data.types[0].type.name;
+            //pokemonData.baseExp = data.base_experience;
+            //pokemonData.sprite = data.sprites.front_default;
+    
+            //pokemones.push(pokemonData);
+          }
+          const request = await Promise.all(arrayRequest);
+    
+          this.setState({
+            pokemonDatas: request.map((pokemon) => ({
+              name: pokemon.name,
+              type: pokemon.types[0].type.name,
+              baseExp: pokemon.base_experience,
+              sprite: pokemon.sprites.front_default,
+            })),
+          });
+    
+        } catch (error) {
+          console.log(error);
+        }
+    }
     render() {
         return (
             <div className='Pokedex'>
                   <h1><img className='Pokedex-icon' alt='Tittle Icon' src={pokeballIcon}></img>Pokedex </h1>
-                  <div className='Pokecard-container'>
-                    { allPokemonData.map( (pkmndata, index) => (
+                  <div className='Pokecard-container'> 
+                    {this.state.pokemonDatas.map( (pkmndata, index) => (
                         <Pokecard key={index}
                          name = { capitalizeFirstLetter(pkmndata.name) } 
                          type = { capitalizeFirstLetter(pkmndata.type) } 
